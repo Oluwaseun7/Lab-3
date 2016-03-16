a43d5147b0ccca6b3a3d83a5c1404967abbc3190
Oluwaseun7 <akinnusotuo@yahoo.com>
2016-03-16 13:15:59 -0400

    Lab 3 and the report
    Uploading Lab 3 and the report

---
+++ Lab #3.py
@@ -0,0 +1,18 @@
+#lab03-Akinnusotu
+def mirrors(myvariable):
+    dna=list(myvariable)
+    for i in range(0, 7):
+        if list[i]==A:
+            return "T"
+        elif list[i]==T:
+            return "A"
+        elif list[i]==C:
+            return "G"
+        elif list[i]==G:
+            return "C"
+        
+bna="ATCGTA"
+
+print(mirrors(bna))
+
+
